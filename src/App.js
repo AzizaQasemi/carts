afdb2453fd35df062c7e983e9d0a10a66783764c
AzizaQasemi <161942280+AzizaQasemi@users.noreply.github.com>
2024-05-10 11:40:25 +0600

    I created carts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import 'bulma/css/bulma.css'
 import './App.css';
+import  Course  from './Course';
+import Angular from './image/download copy.jpeg';
+import Bootstrap from './image/download.jpeg';
+import Csharp from './image/download.png';
+import Kompleweb from './image/images.jpeg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Kurslarim</p>
+        </div>
+      </section>
+      <div className='container'>
+        <section className='section'>
+        <div className='columns'>
+          <div className='column'>
+            <Course 
+             image={Angular}
+             title="Angular" 
+             description="Adds React debugging tools to the Chrome Developer Tools.
+             Created from revision 1717ab0171 on 5/8/2024.
+             React Developer Tools is a Chrome DevTools extension for the open-source React JavaScript library. 
+             It allows you to inspect the React component hierarchies in the Chrome Developer Tools."
+            />
+          </div>
+          <div className='column'>
+            <Course 
+             image={Bootstrap}
+             title="Bootstrap 5" 
+             description="Adds React debugging tools to the Chrome Developer Tools.
+             Created from revision 1717ab0171 on 5/8/2024.
+             React Developer Tools is a Chrome DevTools extension for the open-source React JavaScript library. 
+             It allows you to inspect the React component hierarchies in the Chrome Developer Tools."
+            />
+          </div>
+          <div className='column'>
+            <Course
+             image={Csharp}
+             title="Komple Web" 
+             description="Adds React debugging tools to the Chrome Developer Tools.
+             Created from revision 1717ab0171 on 5/8/2024.
+             React Developer Tools is a Chrome DevTools extension for the open-source React JavaScript library. 
+             It allows you to inspect the React component hierarchies in the Chrome Developer Tools."
+            />
+          </div>
+          <div className='column'>
+            <Course
+             image={Kompleweb}
+             title="Frontend" 
+             description="Adds React debugging tools to the Chrome Developer Tools.
+             Created from revision 1717ab0171 on 5/8/2024.
+             React Developer Tools is a Chrome DevTools extension for the open-source React JavaScript library. 
+             It allows you to inspect the React component hierarchies in the Chrome Developer Tools."
+            />
+          </div>
+        </div>
+        </section>
+    
+      </div>  
     </div>
   );
 }
